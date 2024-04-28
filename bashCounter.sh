278c8a6851d9482909f84558120f9c62319feb04
Tammy Lin <tammy@Tammys-MacBook-Pro.local>
2024-04-28 02:50:57 -0400

    comments

--- bashCounter.sh
+++ bashCounter.sh
@@ -3,8 +3,8 @@
 count=0
 
 while true; do
-    read -rsn1 key
-    if [ -z "$key" ]; then
+    read -rsn1 key # no back slash, reads one character
+    if [ -z "$key" ]; then # check if length 0
         count=$((count+1))
         echo "Enter key pressed $count times"
     fi
